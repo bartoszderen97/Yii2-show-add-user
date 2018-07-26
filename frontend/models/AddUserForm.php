458d97e79fc9bea04b233b54ce58e59f7c4caf9b
unknown <bartoszderen97@gmail.com>
2018-07-26 12:36:07 +0200

    Initial commit

---
+++ frontend/models/AddUserForm.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+use yii\base\Model;
+
+class AddUserForm extends Model
+{
+    public $name;
+    public $job;
+
+    public function rules()
+    {
+        return [
+            [['name', 'job'], 'required'],
+            [['name', 'job'], 'string', 'max' => 20],
+        ];
+    }
+}
---
+++ frontend/views/site/say.php
@@ -0,0 +1,4 @@
+<?php
+use yii\helpers\Html;
+?>
+<?= Html::encode("https://reqres.in/api/users/".$message) ?>
